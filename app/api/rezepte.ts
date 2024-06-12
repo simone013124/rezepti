73b9ff3b3e4cdae2f769df69d4a39e17681df797
Simone <s2210456017@fhooe.at>
2024-06-12 11:54:16 +0200

    fetch data

---
+++ app/api/rezepte.ts
@@ -0,0 +1,27 @@
+import { queryOptions } from '@tanstack/react-query';
+import { Rezept } from '~/models/rezept';
+
+export async function fetchRezepte() {
+    const response = await fetch('www.themealdb.com/api/json/v1/1/search.php?f=a');
+    const rezepte: Rezept[] = await response.json();
+
+    return rezepte;
+}
+
+
+export async function createRezept(title: string) {
+    const response = await fetch('www.themealdb.com/api/json/v1/1/search.php?f=a', {
+        method: 'POST',
+        body: JSON.stringify({ title: title }),
+    });
+    const newRezept: Rezept = await response.json();
+
+    return newRezept;
+}
+
+
+export const playlistQueryOptions = queryOptions({
+    queryKey: ['rezepte'],
+    queryFn: () => fetchRezepte(),
+});
+
