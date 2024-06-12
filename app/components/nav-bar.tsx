55724543804eef777549094705d49ba5fc830b72
Simone <s2210456017@fhooe.at>
2024-06-12 13:42:47 +0200

    fetch data

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -15,9 +15,7 @@ export function NavBar() {
         <Link to="/merkliste" className="px-3 py-1 font-medium hover:bg-accent rounded-sm">
           Merkliste
         </Link>
-        <Link to="/app.rezepte._index" className="px-3 py-1 font-medium hover:bg-accent rounded-sm">
-          Rezepte
-        </Link>
+
       </nav>
     </header>
   );
