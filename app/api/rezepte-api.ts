1c052006aab5709b4041de61b9a624b327ec7051
Simone <s2210456017@fhooe.at>
2024-06-12 13:34:08 +0200

    fetch data

--- app/api/rezepte-api.ts
+++ app/api/rezepte-api.ts
@@ -2,7 +2,7 @@ import { queryOptions } from '@tanstack/react-query';
 import { Rezept } from '~/models/rezept';
 
 export async function fetchRezepte() {
-    const response = await fetch('www.themealdb.com/api/json/v1/1/search.php?f=a');
+    const response = await fetch('http://www.themealdb.com/api/json/v1/1/search.php?f=a');
     const rezepte: Rezept[] = await response.json();
 
     return rezepte;
@@ -10,7 +10,7 @@ export async function fetchRezepte() {
 
 
 export async function createRezept(title: string) {
-    const response = await fetch('www.themealdb.com/api/json/v1/1/search.php?f=a', {
+    const response = await fetch('http://www.themealdb.com/api/json/v1/1/search.php?f=a', {
         method: 'POST',
         body: JSON.stringify({ title: title }),
     });
--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -2,7 +2,7 @@ export default function AppHome() {
   return (
     <>
       <h1>Home</h1>
-      <p className="text-muted-foreground text-sm">Your home. Your music.</p>
+      <p className="text-muted-foreground text-sm">Your home. Your mmmusic.</p>
 
       <div className="mt-5"></div>
     </>
